77d3c5e7680355a382f3c4234e18ed37220febc5
Faturrahman Alharitszoe <72615238+faturrahmanalharitszoe@users.noreply.github.com>
2022-10-03 20:48:37 +0700

    Add files via upload
    Header untuk setiap file php.
    Cara menggunakannya cukup include file ini ke setiap body dari file php lainnya sebelum isi utama dari halaman php yang dibuat.

---
+++ header.php
@@ -0,0 +1,17 @@
+<header>
+        <div class="px-3 py-2">
+            <div class="container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr 2fr;">
+                <a class="nav-link d-inline-block" href="#" style="font-family:'Nunito';font-size:40px;color:#FC4C02;"><strong>TITIPIN</strong></a>
+                <div class="d-flex align-items-center">
+                    <div class="input-group w-50">
+                        <input class="form-control" type="search" placeholder="" aria-label="Search">
+                        <button class="btn" style="background-color:#FC4C02;" type="submit"><img src="./search.png" style="width: 32px;" /></button>
+                    </div>
+                    <button class="btn ms-3 d-inline-block" type="submit"><img src="./heart.png" /></button>
+                    <button class="btn me-auto d-inline-block" type="submit"><img src="./basket.png" /></button> 
+                    <button class="btn d-inline-block" type="submit" style="font-family:'Nunito';font-size:20px;color:#FC4C02;">Sign In</button> 
+                    <button class="btn d-inline-block" type="submit" style="font-family:'Nunito';font-size:20px;color:#FC4C02;">Register</button> 
+                </div>
+            </div>
+        </div>
+</header>
