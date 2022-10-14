b8ee3a23c207f1202a6c0bbf074546f5b15faa55
Nivlake <sadiratarunacalvindoro@gmail.com>
2022-10-14 10:34:17 +0700

    test

--- db_login.php
+++ db_login.php
@@ -4,7 +4,7 @@
 	$db_username = 'root';
 	$db_password = '';
 
-
+	// connect database
 	$db = new mysqli($db_host, $db_username, $db_password, $db_database);
 	if ($db->connect_errno){
 		die ("Tidak menghubungkan database: <br />". $db -> connect_error);
