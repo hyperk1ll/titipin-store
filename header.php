da752198aba917a5a68fe8f84dbd4e9524648de7
Faturrahman Alharitszoe <72615238+faturrahmanalharitszoe@users.noreply.github.com>
2022-10-14 17:04:59 +0700

    update header

--- header.php
+++ header.php
@@ -5,21 +5,21 @@
         if(isset($_SESSION['username'])&&(isset($_SESSION['logged-in']))){?>
         <div class="px-3 py-2">
                 <div class="container-fluid align-items-center" style="grid-template-columns: 1fr 2fr;">
-                    <div class="row">
-                        <div class="col-md">
+                    <div class="row d-flex-row">
+                        <div class="col-sm">
                             <a class="nav-link d-inline-block" href="#" style="font-family:'Nunito';font-size:40px;color:#FC4C02;"><strong>TITIPIN</strong></a>
                         </div>
-                        <div class="col-xl-6" style="width: 30rem;">
+                        <div class="col-xl-6" style="width: 40rem;">
                             <div class="d-flex align-items-center">
                                 <div class="input-group">
                                     <input class="form-control" type="search" placeholder="" aria-label="Search">
                                     <button class="btn" style="background-color:#FC4C02;" type="submit"><img src="./assets/search.png" style="width: 32px;" /></button>
                                 </div>
-                                <button class="btn ms-3 d-inline-block" type="submit"><img src="./assets/heart.png" /></button>
+                                <a href="wishlist_empty.php"><button class="btn ms-3 d-inline-block" type="submit"><img src="./assets/heart.png" /></button></a>
                                 <button class="btn me-auto d-inline-block" type="submit"><img src="./assets/basket.png" /></button> 
                             </div>
                         </div>
-                        <div class="col-md text-end">
+                        <div class="col-md text-end" style="margin-top:5px">
                             <a href="logout.php"><button class="btn d-inline-block" type="submit" style="font-family:'Nunito';font-size:20px;color:#FC4C02;">Logout</button></a>
                         </div>   
                     </div>
@@ -34,19 +34,18 @@
                         <div class="col-md">
                             <a class="nav-link d-inline-block" href="#" style="font-family:'Nunito';font-size:40px;color:#FC4C02;"><strong>TITIPIN</strong></a>
                         </div>
-                        <div class="col-xl-6" style="width: 30rem;">
+                        <div class="col-xl-6" style="width: 40rem;">
                             <div class="d-flex align-items-center">
                                 <div class="input-group">
                                     <input class="form-control" type="search" placeholder="" aria-label="Search">
                                     <button class="btn" style="background-color:#FC4C02;" type="submit"><img src="./assets/search.png" style="width: 32px;" /></button>
                                 </div>
-                                <button class="btn ms-3 d-inline-block" type="submit"><img src="./assets/heart.png" /></button>
+                                <a href="wishlist_empty.php"><button class="btn ms-3 d-inline-block" type="submit"><img src="./assets/heart.png" /></button></a>
                                 <button class="btn me-auto d-inline-block" type="submit"><img src="./assets/basket.png" /></button> 
                             </div>
                         </div>
-                        <div class="col-md text-end">
+                        <div class="col-md text-end" style="margin-top:5px">
                             <a href="login.php"><button class="btn d-inline-block" type="submit" style="font-family:'Nunito';font-size:20px;color:#FC4C02;">Sign In</button></a>
-                            <a href="logout.php"><button class="btn d-inline-block" type="submit" style="font-family:'Nunito';font-size:20px;color:#FC4C02;">Logout</button></a>
                         </div>   
                     </div>
                 </div>
