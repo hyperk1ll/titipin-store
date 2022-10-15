928b1a4ee1b8d961dbbccec4e66448c9a268d615
Faturrahman Alharitszoe <faturrahmanalharitszoe@gmail.com>
2022-10-15 12:31:53 +0700

    buat profil

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function uploadFile(){
     document.getElementById('fileInput').click();
 }
+function inlineEdit(){
+    var t = document.getElementById("table");
+    var trs = t.getElementsByTagName("tr");
+    var tds = null;
+    for (var i=0; i<trs.length; i++){
+        tds = trs[i].getElementsByTagName("td");
+        for (var n=0; n<tds.length;n++)
+        {
+            tds[n].contentEditable = "true";
+        }
+    }
+}
