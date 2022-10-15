788c14cd2641c1460038d0d5d521f19aa2ae78de
Faturrahman Alharitszoe <faturrahmanalharitszoe@gmail.com>
2022-10-15 12:08:32 +0700

    Update Upload Profile

--- profile.php
+++ profile.php
@@ -35,8 +35,14 @@
                 <div class="col-md text-end" style="margin-top:80px;">
                 </div>
                 <div class="col-md-6 d-flex flex-row" id="dataUser" style="margin-top:70px;">
-                    <div>
+                    <div class="d-flex flex-column">
                         <img src="./assets/profile.png" class="rounded-circle" alt="width:200px;height:200px;"style="margin:0 20px 0 0;"></img>
+                        <div style="height:0px;width:0px;overflow: hidden;" >
+                            <input type="file" id="fileInput" >
+                        </div>
+                        <div class="d-flex justify-content-center" style="margin-right:20px;">
+                            <button class="btn w-75" type="button" style="background-color:#FC4C02;margin-top:3px;color:white;" onclick="uploadFile();">Choose File</button>
+                        </div>
                     </div>
                     <div class="d-inline-block" style="margin: 40px 0 0px 0px;">
                         <table>
@@ -57,6 +63,9 @@
                                 <td>: &nbsp;&nbsp;&nbsp;<?php echo $row['no_telepon'] ?></td>
                             </tr>
                         </table>
+                        <div class="d-flex justify-content-end">
+                        <button class="btn btn-sm d-inline-block" type="submit" style="--bs-btn-padding-y: .10rem; --bs-btn-padding-x: .5rem;font-family:'Nunito';font-size:20px;background-color:#FC4C02;color:white;margin-top:5px;"><i class="fa-solid fa-pen-to-square" style="margin-right:3px;" ></i>Edit</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-sm">
@@ -66,7 +75,7 @@
     </div>
 
     <?php include('./footer.php'); ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
-    
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  <script src="./script.js" ></script>    
 </html>
