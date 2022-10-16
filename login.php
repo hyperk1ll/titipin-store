f895b5f453f34fdb8a2948bf6362f8986e08c038
Hilmy Tsany <82049652+hyperk1ll@users.noreply.github.com>
2022-10-16 15:21:50 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,9 +2,43 @@
 	session_start();
 	require_once('db_login.php');
 
-	
+	if (isset($_POST["submit"])){
+		$valid = TRUE;
+		
+		$username = test_input($_POST['user']);
+		if ($username== ''){
+			$error_username = "Username is required";
+			$valid = FALSE;
+		} 
+		
+		$password = test_input($_POST['pass']);
+		if ($password == ''){
+			$error_password = "Password is required";
+			$valid = FALSE;
+		}
+		
+		if ($valid){
+			$query = " SELECT * FROM user WHERE username='".$username."' AND password=MD5('".$password."') ";
+			$result = $db->query($query);
+			if (!$result){
+				die ("Could not query the database: <br />". $db->error);
+			} 
+			else{
+				if ($result->num_rows>0){ 
+					$_SESSION['username'] = $username;
+					$_SESSION['logged-in'] = true;
+					header("Location: index.php?=user".$_SESSION['username']); //ganti ini kalo mau redirect ke halaman lain
+					exit;
+				} 
+				else{
+					echo '<span class="error">Combination of username and password are not correct.</span>';
+				}
+			}
+			
+			$db->close();
+		}
+	}
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,53 +53,13 @@
 
 <body style = "background-color:#F7F7F7;"> <!-- warnain baground abu-abu -->
 	<br>
+	<div class = "jumbotron text-center"> <!-- Buat nengahin tulisan TITIPIN -->
+		<a class = "nav-link d-inline-block me-auto" style = "font-family:'Nunito'; font-size:50px; color:#FC4C02;"> 
+			<strong> TITIPIN </strong> 
+		</a>
+	</div>
+	
 	<div class = "container">
-	<section class="row justify-content-center">
-		<?php
-		if (isset($_POST["submit"])){
-			$valid = TRUE;
-			
-			$username = test_input($_POST['user']);
-			if ($username== ''){
-				$error_username = "Username is required";
-				$valid = FALSE;
-			} 
-			
-			$password = test_input($_POST['pass']);
-			if ($password == ''){
-				$error_password = "Password is required";
-				$valid = FALSE;
-			}
-			
-			if ($valid){
-				$query = " SELECT * FROM user WHERE username='".$username."' AND password='".$password."' ";
-				$result = $db->query($query);
-				if (!$result){
-					die ("Could not query the database: <br />". $db->error);
-				} 
-				else{
-					if ($result->num_rows>0){ 
-						$_SESSION['username'] = $username;
-						$_SESSION['logged-in'] = true;
-						header("Location: index.php?=user".$_SESSION['username']); //ganti ini kalo mau redirect ke halaman lain
-						exit;
-					} 
-					else{
-						echo '<div class="alert alert-danger" style="font-family:"Nunito";">Email atau Password salah!</div>';
-					}
-				}
-	
-	
-				$db->close();
-			}
-		}
-
-		?>
-		<div class = "jumbotron text-center"> <!-- Buat nengahin tulisan TITIPIN -->
-			<a class = "nav-link d-inline-block me-auto" style = "font-family:'Nunito'; font-size:50px; color:#FC4C02;"> 
-				<strong> TITIPIN </strong> 
-			</a>
-		</div>
 		<div class = "card" style = "width: 350px; margin: 0 auto;"> <!-- biar kotak putih gak melebar pas dibesarin -->
 		<div class = "card-body">
 		<form method = "POST" autocomplete = "on" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
@@ -82,15 +76,13 @@
 				<a href = "register.php" style="font-family:'Nunito'; color:#FC4C02; text-decoration:none;"> Don't have an account? </button> <br> <br>
 				
 				<div class="col-md-12 text-center"> <!-- Buat nengahin tombol -->
-					<button class = "btn btn-primary" type = "submit" name = "submit" value = "submit"> Login </button>
+					<button class = "tombol" type = "submit" name = "submit" value = "submit"> Login </button>
 				</div>
 			</div>
 		</form>
 		</div>
 		</div>
-	</section>
 	</div>
-	
 	<script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity ="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin = "anonymous"> </script>
 </body>
 </html>
